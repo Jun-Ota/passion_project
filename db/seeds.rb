17090d99c42bc9011534f04707eba7b2369770d2
Jun Ota <email": null>
2017-06-15 12:23:15 -0700

    add seed file

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+require 'faker'
+  number_of_users_wanted = 20
+  new_users_needed = number_of_users_wanted - User.count
+
+new_users_needed.times do
+   user = { user_name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Coffee.blend_name }
+   user = User.new(user)
+   user.save
+end
+
+
+ number_of_kudos_wanted = 20
+  new_kudos_needed = number_of_kudos_wanted - Kudo.count
+
+new_kudos_needed.times do
+   kudo = { user_name: Faker::Name.name, subject: Faker::Book.title, description: Faker::Coffee.blend_name, applause: rand(1..20) }
+   kudo = Kudo.new(user)
+   kudo.save
+end
+
+
+
